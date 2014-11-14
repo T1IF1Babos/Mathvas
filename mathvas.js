71fdb09f650040bc7b1ff99050b8f0aa4fd359ab
RedCrafter1 <hendeljo@yahoo.de>
2014-11-14 21:38:35 +0100

    defining canvas

--- mathvas.js
+++ mathvas.js
@@ -1,10 +1,14 @@
 /**
  * Created by VesAl & HenJo on 14/11/2014.
  */
+
 "use strict";
 
 var canvas, context;
 
+/**
+ * Funktion wird ausgeführt, wenn das gesammte HTML Dokument geladen ist
+ */
 function init() {
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
