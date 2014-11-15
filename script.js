e66986ded61d0defa50261579ef32db2f33071ed
ScaryBlock <lux.boy.serb@gmail.com>
2014-11-15 19:59:41 +0100

    Axis UPDATE BIATCHES!!!!!

--- script.js
+++ script.js
@@ -28,15 +28,40 @@ function setVariables() {
 	canvasCenterY = canvasHeight / 2;
 }
 
+/**
+ * X und Y Axe zeichnen!
+ */
 function drawAxis(){
     context.fillStyle = "#000000";
     context.fillRect(canvasWidth / 2, 0, 1, canvasHeight);
     context.fillRect(0, canvasHeight / 2, canvasWidth, 1);
-
-    for (var i = canvasCenterX; i > 0; i -= pixelDistance) {context.fillRect(i, canvasCenterY - 3, 1,7);}
-    for (i = canvasCenterX; i < canvasWidth; i += pixelDistance) {context.fillRect(i, canvasCenterY - 3, 1, 7);}
-    for (i = canvasCenterY; i > 0; i -= pixelDistance) {context.fillRect(canvasCenterX - 3, i, 7, 1);}
-    for (i = canvasCenterY; i < canvasHeight; i += pixelDistance) {context.fillRect(canvasCenterX - 3, i, 7, 1);}
+	context.font = "10px Arial";
+	context.textBaseline = "middle";
+	context.textAlign = "center";
+	var distance = 0;
+	for (var i = canvasCenterX; i > 0; i -= pixelDistance) {
+		context.fillRect(i, canvasCenterY - 3, 1, 7);
+		if (distance != 0) context.fillText(distance + "", i, canvasCenterY + 10);
+		distance -= 1;
+	}
+	distance = 0;
+	for (i = canvasCenterX; i < canvasWidth; i += pixelDistance) {
+		context.fillRect(i, canvasCenterY - 3, 1, 7);
+		if (distance != 0) context.fillText(distance + "", i, canvasCenterY + 10);
+		distance += 1;
+	}
+	distance = 0;
+	for (i = canvasCenterY; i > 0; i -= pixelDistance) {
+		context.fillRect(canvasCenterX - 3, i, 7, 1);
+		if (distance != 0) context.fillText(distance + "", canvasCenterX - 10, i);
+		distance -= 1;
+	}
+	distance = 0;
+	for (i = canvasCenterY; i < canvasHeight; i += pixelDistance) {
+		context.fillRect(canvasCenterX - 3, i, 7, 1);
+		if (distance != 0) context.fillText(distance + "", canvasCenterX - 10, i);
+		distance += 1;
+	}
 }
 
 window.addEventListener("load", init);
