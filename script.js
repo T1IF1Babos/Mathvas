b6b94dfa9f73bf23a3d0a5884b0bd3eddb6e06da
ScaryBlock <lux.boy.serb@gmail.com>
2014-11-18 17:51:45 +0100

    Raster hinzugefügt!

--- script.js
+++ script.js
@@ -37,6 +37,8 @@ function setVariables() {
  * Funktion zeichnet die X und Y Axe ein
  */
 function drawAxis(){
+	context.fillStyle = "#ffffff";
+	context.fillRect(0, 0, canvasWidth, canvasHeight);
 	context.fillStyle = "#000000";
 	context.fillRect(canvasWidth / 2, 0, 1, canvasHeight);
 	context.fillRect(0, canvasHeight / 2, canvasWidth, 1);
@@ -44,29 +46,44 @@ function drawAxis(){
 	context.textBaseline = "middle";
 	context.textAlign = "center";
 
+	var pointsX = [];
+	var pointsY = [];
 	var distance = 0;
 	for (var i = canvasCenterX; i > 0; i -= pixelDistance) {
 		context.fillRect(i, canvasCenterY - 3, 1, 7);
 		if (distance != 0) context.fillText(distance + "", i, canvasCenterY + 10);
 		distance -= 1;
+		pointsX.push(i);
 	}
 	distance = 0;
 	for (i = canvasCenterX; i < canvasWidth; i += pixelDistance) {
 		context.fillRect(i, canvasCenterY - 3, 1, 7);
 		if (distance != 0) context.fillText(distance + "", i, canvasCenterY + 10);
 		distance += 1;
+		pointsX.push(i);
 	}
 	distance = 0;
 	for (i = canvasCenterY; i > 0; i -= pixelDistance) {
 		context.fillRect(canvasCenterX - 3, i, 7, 1);
 		if (distance != 0) context.fillText(distance + "", canvasCenterX - 10, i);
 		distance += 1;
+		pointsY.push(i);
 	}
 	distance = 0;
 	for (i = canvasCenterY; i < canvasHeight; i += pixelDistance) {
 		context.fillRect(canvasCenterX - 3, i, 7, 1);
 		if (distance != 0) context.fillText(distance + "", canvasCenterX - 10, i);
 		distance -= 1;
+		pointsY.push(i);
+	}
+	context.fillStyle = "#8B8B8B";
+	for (var x = 0; x < pointsX.length; x++) {
+		if (pointsX[x] == canvasCenterX) continue;
+		for (var y = 0; y < pointsY.length; y++) {
+			if (pointsY[y] == canvasCenterY) continue;
+			context.fillRect(pointsX[x] - 2, pointsY[y], 5, 1);
+			context.fillRect(pointsX[x], pointsY[y] - 2, 1, 5);
+		}
 	}
 }
 
@@ -92,7 +109,7 @@ function drawParabole(a, b, c) {
 		x = (i - canvasCenterX) / pixelDistance;
 
 		y = a * x * x + b * x + c;
-		y = (y * -1 * pixelDistance) + canvasCenterY;
+		y = y * -1 * pixelDistance + canvasCenterY;
 
 		if (y + pixelDistance < 0 || y - pixelDistance > canvas.width) continue;
 
